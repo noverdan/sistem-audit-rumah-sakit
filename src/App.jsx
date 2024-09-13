cf32425768574745a1c9728a65b74b8b3a7e9142
Gindadwi <gindadwi144@gmail.com>
2024-09-13 22:31:48 +0700

    wip: create initial login page layout
    added the UI layout for the login page with basic styling. Functionality such as API connection and form validation is still pending.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import IndexManager from "./pages/manager/Index";
 import IndexPerawat from "./pages/perawat/Index";
+import Login from "./pages/Login"
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
             <Routes>
                 <Route path="/p/*" element={<IndexPerawat />} />
                 <Route path="/m/*" element={<IndexManager />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </main>
     );
