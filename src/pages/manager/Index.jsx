d4ac37852ccb31d3ca6903b582433f67fd17ca8f
Galang Arsandy <galangarsandy@gmail.com>
2024-09-13 18:03:07 +0700

    feat: Add default route redirection for manager and perawat pages

--- src/pages/manager/Index.jsx
+++ src/pages/manager/Index.jsx
@@ -1,5 +1,5 @@
 import Sidebar from "../../components/layout/Sidebar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import DashboardManager from "./Dashboard";
 import Pathway from "./Pathway";
 import Laporan from "./Laporan";
@@ -12,11 +12,13 @@ function IndexManager() {
                 <p>Manager</p>
             </Sidebar>
             <Routes>
+                <Route path="/" element={<Navigate to={"/m/dashboard"} />} />
                 <Route path="/dashboard" element={<DashboardManager />} />
                 <Route path="/pathway" element={<Pathway />} />
                 <Route path="/laporan" element={<Laporan />} />
                 <Route path="/managemen-akun" element={<ManagemenAkun />} />
                 <Route path="/data-dashboard" element={<DataDashboard />} />
+                <Route path="/*" element={<Navigate to={"/m/dashboard"} />} />
             </Routes>
         </>
     );
--- src/pages/perawat/Index.jsx
+++ src/pages/perawat/Index.jsx
@@ -2,7 +2,7 @@ import Sidebar from "../../components/layout/Sidebar";
 import DashboardPerawat from "./Dashboard";
 import DataPasien from "./DataPasien";
 import StatusPasien from "./StatusPasien";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function IndexPerawat() {
     return (
@@ -11,9 +11,11 @@ function IndexPerawat() {
                 <p>Perawat</p>
             </Sidebar>
             <Routes>
+                <Route path="/" element={<Navigate to={"/p/dashboard"} />} />
                 <Route path="/dashboard" element={<DashboardPerawat />} />
                 <Route path="/data-pasien" element={<DataPasien />} />
                 <Route path="/status-pasien" element={<StatusPasien />} />
+                <Route path="/*" element={<Navigate to={"/p/dashboard"} />} />
             </Routes>
         </>
     );
