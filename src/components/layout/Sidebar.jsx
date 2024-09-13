9889bc55a453b0c0d2e9f5261019542d83b82120
Galang Arsandy <galangarsandy@gmail.com>
2024-09-13 22:04:55 +0700

    feat: Add Sidebar component for navigation menu
    Add new file Sidebar.jsx in the components/layout directory to implement the sidebar navigation menu. The Sidebar component receives a list of navigation items as props and renders them as NavLink components. Each navigation item consists of a title, path, and icon. This allows users to easily navigate between different pages within the application. The Sidebar component also includes a logo and a tagline for the application.
    
    The Sidebar component is used in the IndexManager and IndexPerawat components to display the sidebar menu for the manager and perawat pages respectively. The navList prop is passed to Sidebar component with the appropriate navigation items for each user role.

---
+++ src/components/layout/Sidebar.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import { Icon } from "@iconify/react"
+
+export default function Sidebar({ navList }) {
+
+    const btnSidebarStyle = "flex min-h-11 items-center p-[10px] font-medium gap-2 transition duration-200";
+    const navActive = ({ isActive }) => {
+        return isActive ? "bg-primary-3 text-white " + btnSidebarStyle : "text-gray-500 hover:bg-stroke " + btnSidebarStyle;
+    }
+
+    return (
+        <aside className="bg-white h-svh min-w-72 border-r border-stroke p-6 box-border flex flex-col">
+            <div id="sidebar-logo" className="flex flex-col gap-1">
+                <h1 className="text-primary-3 font-semibold text-4xl">SQUARE</h1>
+                <p className="text-primary-3 text-[8px]">(Stroke Quality Assurance and Care)</p>
+            </div>
+            <nav id="sidebar-nav" className="flex gap-2 flex-col mt-8">
+                {
+                    navList?.map((nav, index) => (
+                        <NavLink
+                            key={index}
+                            to={nav.path}
+                            className={({ isActive }) => navActive({ isActive })}
+                        >
+                            <Icon icon={nav.iconify} width={20} />
+                            <p>{nav.title}</p>
+                        </NavLink>
+                    ))
+                }
+            </nav>
+        </aside>
+    )
+}
+
+Sidebar.propTypes = {
+    navList: PropTypes.arrayOf(PropTypes.object)
+};
--- src/pages/perawat/Index.jsx
+++ src/pages/perawat/Index.jsx
@@ -4,12 +4,29 @@ import DataPasien from "./DataPasien";
 import StatusPasien from "./StatusPasien";
 import { Route, Routes, Navigate } from "react-router-dom";
 
+const navList = [
+    {
+        title: "Dashboard",
+        path: "/p/dashboard",
+        iconify: "hugeicons:dashboard-square-01"
+    },
+    {
+        title: "Data Pasien",
+        path: "/p/data-pasien",
+        iconify: "carbon:data-1"
+    },
+    {
+        title: "Status Pasien",
+        path: "/p/status-pasien",
+        iconify: "uil:chart"
+    }
+
+]
+
 function IndexPerawat() {
     return (
         <>
-            <Sidebar>
-                <p>Perawat</p>
-            </Sidebar>
+            <Sidebar navList={navList} />
             <Routes>
                 <Route path="/" element={<Navigate to={"/p/dashboard"} />} />
                 <Route path="/dashboard" element={<DashboardPerawat />} />
