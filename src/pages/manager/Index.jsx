0584a917ba85da2b2b2083a870db22b5ecc91216
Galang Arsandy Noverdan Putra <124428540+noverdan@users.noreply.github.com>
2024-09-13 22:19:08 +0700

    Feature/glg sidebar layout (#2)
    * feat: Add routing and pages for manager and perawat
    
    This commit adds routing and page components for the manager and perawat roles. It introduces the following changes:
    - Added IndexManager component for the manager role
    - Added DashboardManager, DataDashboard, Laporan, ManagemenAkun, and Pathway components for the manager role
    - Added IndexPerawat component for the perawat role
    - Added DashboardPerawat, DataPasien, and StatusPasien components for the perawat role
    
    These changes enable the application to display different pages based on the user's role, providing a more tailored experience.
    
    * feat: Add default route redirection for manager and perawat pages
    
    * feat: Add Sidebar component for navigation menu
    
    Add new file Sidebar.jsx in the components/layout directory to implement the sidebar navigation menu. The Sidebar component receives a list of navigation items as props and renders them as NavLink components. Each navigation item consists of a title, path, and icon. This allows users to easily navigate between different pages within the application. The Sidebar component also includes a logo and a tagline for the application.
    
    The Sidebar component is used in the IndexManager and IndexPerawat components to display the sidebar menu for the manager and perawat pages respectively. The navList prop is passed to Sidebar component with the appropriate navigation items for each user role.

--- src/pages/manager/Index.jsx
+++ src/pages/manager/Index.jsx
@@ -5,18 +5,46 @@ import Pathway from "./Pathway";
 import Laporan from "./Laporan";
 import ManagemenAkun from "./ManagemenAkun";
 import DataDashboard from "./DataDashboard";
+
+const navList = [
+    {
+        title: "Dashboard",
+        path: "/m/dashboard",
+        iconify: "hugeicons:dashboard-square-01"
+    },
+    {
+        title: "Pathway",
+        path: "/m/pathway",
+        iconify: "material-symbols:conversion-path"
+    },
+    {
+        title: "Kelola Laporan",
+        path: "/m/laporan",
+        iconify: "uil:chart"
+    },
+    {
+        title: "Manajemen Akun Perawat",
+        path: "/m/manajemen-akun",
+        iconify: "lucide:users-round"
+    },
+    {
+        title: "Data Dashboard",
+        path: "/m/data-dashboard",
+        iconify: "solar:pie-chart-2-linear"
+    }
+
+]
+
 function IndexManager() {
     return (
         <>
-            <Sidebar>
-                <p>Manager</p>
-            </Sidebar>
+            <Sidebar navList={navList} />
             <Routes>
                 <Route path="/" element={<Navigate to={"/m/dashboard"} />} />
                 <Route path="/dashboard" element={<DashboardManager />} />
                 <Route path="/pathway" element={<Pathway />} />
                 <Route path="/laporan" element={<Laporan />} />
-                <Route path="/managemen-akun" element={<ManagemenAkun />} />
+                <Route path="/manajemen-akun" element={<ManagemenAkun />} />
                 <Route path="/data-dashboard" element={<DataDashboard />} />
                 <Route path="/*" element={<Navigate to={"/m/dashboard"} />} />
             </Routes>
