f175ec96adba4960c24f13b9c65f4d44f67be4c5
Galang Arsandy <galangarsandy@gmail.com>
2024-09-14 22:08:09 +0700

    refactor: add toast notification and loading state to login page, and also tried using mockApi for auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import IndexManager from "./pages/manager/Index";
 import IndexPerawat from "./pages/perawat/Index";
 import Login from "./pages/Login"
 import { Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 function App() {
     return (
         <main className="flex">
+            <Toaster position="top-center" toastOptions={{ duration: 1500 }} />
             <Routes>
                 <Route path="/p/*" element={<IndexPerawat />} />
                 <Route path="/m/*" element={<IndexManager />} />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,54 +2,51 @@ import { useState } from "react";
 import Button from "../components/common/Button"
 import axios from "axios";
 import { Icon } from '@iconify/react';
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-    const [username, setUsername] = useState();
-    const [password, setPassword] = useState();
-    const [error, setError] = useState();
     const [showPassword, setShowPassword] = useState();
     const [inputError, setInputError] = useState({ username: false, password: false });
+    const [isLoading, setIsLoading] = useState(false);
+    const [input, setInput] = useState({ username: "", password: "" });
+    const navigate = useNavigate();
+
 
     const submitData = async (e) => {
         e.preventDefault();
-        setError('Periksa kata sandi dan username')
-        setInputError({ username: false, password: false });
 
-        if (!username || !password) {
-            setInputError({
-                username: !username,
-                password: !password,
-            })
-            setError("Mohon isi semua field");
+        if (!input.username || !input.password) {
+            toast.error("Masukan username dan password");
+            setInputError({ username: !input.username, password: !input.password });
             return;
         }
 
         try {
-            const response = await axios.post('https://example.com/api/login', {
-                username,
-                password,
-            });
-
-            // Mengambil token dan role dari respons api
-            const { token, role } = response.data;
-
-            // simpan token di localstorage 
-            localStorage.setItem('token', token);
-
-            // mengarahkan pengguna berdasarkan role
-            if (role == 'perawat') {
-                window.location.href = '/';
-            } else if (role === 'manager') {
-                window.location.href = '/'
-            } else {
-                setError('Pengguna Tidak dikenal')
-            }
-
+            setIsLoading(true);
+            const response = await axios.post("https://dummyjson.com/auth/login", input);
+            localStorage.setItem("token", response.data.token);
+            authMe()
+                .then((data) => {
+                    toast.success("Login berhasil");
+                    // Dummy role from mockapi
+                    if (data.role === "admin") {
+                        navigate("/m/dashboard");
+                    } else if (data.role === "user") {
+                        navigate("/p/dashboard");
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+                .finally(() => {
+                    setIsLoading(false);
+                });
         } catch (error) {
-            console.error('Error logging in:', error);
+            console.log(error);
+            toast.error("Error: " + error.message);
+            setIsLoading(false);
         }
-
-
     }
 
 
@@ -69,10 +66,11 @@ export default function Login() {
                                     type="text"
                                     name="username"
                                     placeholder=" "
-                                    onChange={(e) => setUsername(e.target.value)}
-                                    className={`border border-black border-opacity-50 outline-none rounded-2xl w-full h-12 pl-4 
-                                             focus:text-black transition duration-200 ${inputError.username ? "input-error" : ""
+                                    onChange={(e) => setInput({ ...input, username: e.target.value })}
+                                    className={`disabled:cursor-not-allowed focus:invalid:border-red-600 border border-black border-opacity-50 outline-none rounded-2xl w-full h-12 pl-4 
+                                             focus:text-black transition duration-200 ${!input.username && inputError.username ? "input-error" : ""
                                         }`}
+                                    disabled={isLoading}
                                 />
                                 <label className="input-text">Username</label>
                             </div>
@@ -82,9 +80,10 @@ export default function Login() {
                                 <input type={showPassword ? "text" : "password"}
                                     name="password"
                                     placeholder=""
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    className={`border border-black border-opacity-50 outline-none 
-                                     rounded-2xl w-full h-12 pl-4 pr-10 ${inputError.username ? "input-error" : ""} `}
+                                    onChange={(e) => setInput({ ...input, password: e.target.value })}
+                                    className={`disabled:cursor-not-allowed border border-black border-opacity-50 outline-none 
+                                     rounded-2xl w-full h-12 pl-4 pr-10 ${!input.password && inputError.password ? "input-error" : ""} `}
+                                    disabled={isLoading}
                                 />
                                 <label className="input-text">Password</label>
                                 <button type="button" className="absolute right-4 top-1/2 -translate-y-1/2"
@@ -99,9 +98,9 @@ export default function Login() {
                                     text={'Login'}
                                     size={'large'}
                                     onClick={submitData}
+                                    disabled={isLoading}
+                                    isLoading={isLoading}
                                 />
-
-                                {error && <p className="mt-4">{error}</p>}
                             </div>
 
                         </form>
@@ -111,4 +110,27 @@ export default function Login() {
 
         </div>
     )
+}
+
+// Dummy authorization
+// visit https://dummyjson.com/users to see a list of dummy users
+async function authMe() {
+    const token = localStorage.getItem("token");
+    if (!token) {
+        console.log("Token not found");
+    } else {
+        try {
+            const response = await axios({
+                method: "GET",
+                url: "https://dummyjson.com/auth/me",
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            return response.data;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
 }
