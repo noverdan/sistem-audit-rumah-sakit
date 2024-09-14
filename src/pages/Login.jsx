22a40319c4840435f7126464721e24e6bcd46052
Gindadwi <gindadwi144@gmail.com>
2024-09-14 16:23:33 +0700

    wip: create initial login page layout
    added the UI layout for the login page with basic styling. Functionality such as API connection and form validation is still pending.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,29 @@
 import { useState } from "react";
-import Button  from "../components/common/Button"
+import Button from "../components/common/Button"
 import axios from "axios";
-import { Await } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import '../index.css'
 
-export default function Login (){
+export default function Login() {
     const [username, setUsername] = useState();
     const [password, setPassword] = useState();
     const [error, setError] = useState();
+    const [showPassword, setShowPassword] = useState();
+    const [inputError, setInputError] = useState({ username: false, password: false });
 
     const submitData = async (e) => {
         e.preventDefault();
-        console.log('asu')
-        setError('error Coy')
+        setError('periksa kata sandi dan username')
+        setInputError({ username: false, password: false });
 
+        if (!username || !password) {
+            setInputError({
+                username: !username,
+                password: !password,
+            })
+            setError("Mohon isi semua field");
+            return;
+        }
 
         try {
             const response = await axios.post('https://example.com/api/login', {
@@ -37,7 +48,6 @@ export default function Login (){
             }
 
         } catch (error) {
-            setError('Login gagal, periksa kembali email atau password Anda.');
             console.error('Error logging in:', error);
         }
 
@@ -45,49 +55,60 @@ export default function Login (){
     }
 
 
-    return(
+    return (
         <div className="bg-gradient-to-tr from-primary-4 to-primary-1 h-screen w-screen flex justify-center items-center">
 
             <div className="bg-white w-96 h-[550px] rounded-3xl p-8 text-center flex justify-center items-center ">
-              <div className="flex flex-col gap-6">
+                <div className="flex flex-col gap-6">
                     <p className="text-3xl font-semibold">Login</p>
                     <p className=" text-xs text-gray-700 ">Selamat datang di Sistem Audit Rumah Sakit,
                         Silahkan pilih jenis login</p>
 
                     <div>
-                        <form  className="flex flex-col gap-6 ">
-                            <input type="text"
-                                name="username"
-                                placeholder="username"
-                                onChange={(e) => setUsername(e.target.value)}
-                                className="border border-black rounded-2xl w-full h-12 pl-4"
-                            />
-
-                            <div>
-                                <input type="text"
-                                    name="password"
-                                    placeholder="password"
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    className="border border-black rounded-2xl w-full h-12 pl-4 "
+                        <form className="flex flex-col gap-6 ">
+                            <div className="relative input-container">
+                                <input
+                                    type="text"
+                                    name="username"
+                                    placeholder=" "
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    className={`border border-black border-opacity-50 outline-none rounded-2xl w-full h-12 pl-4 
+                                             transition duration-200 ${inputError.username ? "input-error" : ""
+                                        }`}
                                 />
-                                
-                                                            
+                                <label className="input-text">Username</label>
                             </div>
 
-                            
+
+                            <div className="relative input-container">
+                                <input type={showPassword ? "text" : "password"}
+                                    name="password"
+                                    placeholder=""
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    className={`border border-black border-opacity-50 outline-none 
+                                     rounded-2xl w-full h-12 pl-4 pr-10 ${inputError.username ? "input-error" : ""} `}
+                                />
+                                <label className="input-text">Password</label>
+                                <button type="button" className="absolute right-4 top-1/2 -translate-y-1/2"
+                                    onClick={() => setShowPassword(!showPassword)}>
+                                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                                </button>
+                            </div>
+
+
                             <div className="flex flex-col items-center justify-center">
                                 <Button
                                     text={'Login'}
                                     size={'large'}
                                     onClick={submitData}
-                                /> 
+                                />
 
-                                {error && <p>{error}</p>}                            
+                                {error && <p className="mt-4">{error}</p>}
                             </div>
 
                         </form>
-                    </div>  
-             </div> 
+                    </div>
+                </div>
             </div>
 
         </div>
