8f7d0ef819fa30e052d062588348b69bc39f187f
Galang Arsandy <galangarsandy@gmail.com>
2024-09-16 21:10:25 +0700

    refactor: Component layout to use a minimum width of 720px

--- src/pages/perawat/DataPasien.jsx
+++ src/pages/perawat/DataPasien.jsx
@@ -2,7 +2,7 @@ import PageHeader from "../../components/layout/PageHeader";
 
 export default function DataPasien() {
     return (
-        <main className="w-full bg-gray-100 min-w-[1024px]">
+        <main className="w-full bg-gray-100 min-w-[720px]">
             <PageHeader title="Data Pasien" />
         </main>
     )
