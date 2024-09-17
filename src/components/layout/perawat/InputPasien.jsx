1c56a8dded49b228b380e69be901f3de8f7032e7
Gindadwi <gindadwi144@gmail.com>
2024-09-17 16:53:49 +0700

    feat: initiate input pasien on page DataPasien

---
+++ src/components/layout/perawat/InputPasien.jsx
@@ -0,0 +1,7 @@
+export default function InputPasien(){
+    return(
+        <div>
+            <h1 className="text-center">INPUT DATA PASIEN</h1>
+        </div>
+    )
+}
--- src/pages/perawat/DataPasien.jsx
+++ src/pages/perawat/DataPasien.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import PageHeader from "../../components/layout/PageHeader";
+import InputPasien from "../../components/layout/perawat/InputPasien";
 
 export default function DataPasien() {
+    const [isInputPasien, setIsInputPasien]= useState(false)
     return (
         <main className="w-full bg-gray-100 min-w-[720px]">
             <PageHeader title="Data Pasien" />
+            {
+                !isInputPasien && (
+                    <button onClick={()=>setIsInputPasien(!isInputPasien)}>Klik</button>
+                )
+            }
+            {
+                isInputPasien && (
+                    <InputPasien />
+                )
+            }
         </main>
     )
 }
