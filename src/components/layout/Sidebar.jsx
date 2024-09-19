0e0363bf11c409244b29ccfcd19e6591e27905d2
Galang Arsandy <galangarsandy@gmail.com>
2024-09-19 19:41:41 +0700

    refactor: sidebar width & sidebar button text size

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -4,14 +4,14 @@ import { Icon } from "@iconify/react"
 
 export default function Sidebar({ navList }) {
 
-    const btnSidebarStyle = "flex min-h-11 items-center p-[10px] font-medium gap-2 transition duration-200";
+    const btnSidebarStyle = "flex min-h-11 text-sm items-center p-[10px] font-medium gap-2 transition duration-200";
     const navActive = ({ isActive }) => {
         return isActive ? "bg-primary-3 text-white " + btnSidebarStyle : "text-gray-500 hover:bg-stroke " + btnSidebarStyle;
     }
 
     return (
         <>
-            <aside className="bg-white fixed h-svh min-w-72 border-r border-stroke p-6 box-border flex flex-col">
+            <aside className="bg-white fixed h-svh min-w-[256px] border-r border-stroke p-6 box-border flex flex-col">
                 <div id="sidebar-logo" className="flex flex-col gap-1">
                     <h1 className="text-primary-3 font-semibold text-4xl">SQUARE</h1>
                     <p className="text-primary-3 text-[8px]">(Stroke Quality Assurance and Care)</p>
@@ -31,7 +31,7 @@ export default function Sidebar({ navList }) {
                     }
                 </nav>
             </aside>
-            <div className='h-svh min-w-72'></div>
+            <div className='h-svh min-w-[256px]'></div>
         </>
     )
 }
